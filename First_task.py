07cf4006317bd887a05c149eb7c750e9ba33c459
marcin-kosz <koszu1995@gmail.com>
2026-01-05 15:04:30 +0100

    Corrections_5.01

--- First_task.py
+++ First_task.py
@@ -2,11 +2,11 @@ from datetime import datetime, date
 
 def get_days_from_today(date_string):
     try:
-        nonstring_date = datetime.strptime(date_string, "%Y.%m.%d").date()
+        nonstring_date = datetime.strptime(date_string, "%Y-%m-%d").date()
         today = date.today()
         days_from_today = nonstring_date - today
         return days_from_today.days
     except ValueError:
-        print("Error: type your date as YYYY.MM.DD")
+        print("Error: type your date as YYYY-MM-DD")
     except TypeError:
         print("Error: type your date as string")
--- Third_task.py
+++ Third_task.py
@@ -2,9 +2,10 @@ import re
 
 def normalize_phone(phone_number):
     normalized_phone = re.sub(r'\D', '', phone_number)
-    if not normalized_phone.startswith('+'):
-        if normalized_phone.startswith('380'):
-            normalized_phone = '+' + normalized_phone
-        else:
-            normalized_phone = '+38' + normalized_phone
-    return normalized_phone
+        
+    if normalized_phone.startswith('380'):
+        normalized_phone = '+' + normalized_phone
+    elif normalized_phone.startswith('0'):
+        normalized_phone = '+38' + normalized_phone
+    else:
+        return '+' + normalized_phone
