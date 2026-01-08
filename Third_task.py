0b8787e45f248e2c6b5f2d88818f9a0d621576f8
marcin-kosz <koszu1995@gmail.com>
2026-01-08 17:31:41 +0100

    Last correction

--- Third_task.py
+++ Third_task.py
@@ -1,11 +1,12 @@
 import re
-
 def normalize_phone(phone_number):
     normalized_phone = re.sub(r'\D', '', phone_number)
-        
     if normalized_phone.startswith('380'):
         normalized_phone = '+' + normalized_phone
+        return normalized_phone
     elif normalized_phone.startswith('0'):
         normalized_phone = '+38' + normalized_phone
+        return normalized_phone
     else:
         return '+' + normalized_phone
+print(normalize_phone("+380441234567"))
